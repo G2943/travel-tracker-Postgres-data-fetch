042d4f78f3fee7222d92117cc69a4c166db04593
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-12-25 18:59:47 +0530

    pass data to index.ejs file

--- index.js
+++ index.js
@@ -15,27 +15,33 @@ const db = new pg.Client({
 
 db.connect();
 
-let countries = [];
-let countryCodes = [];
 
-db.query("select country_code from visited_countries", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    countries = res.rows;
-    countryCodes = countries.map(obj => obj.country_code);
-    console.log("rows are: ", countryCodes);
-    // quiz = res.rows;
-  }
-  db.end();
-});
+// db.query("select country_code from visited_countries", (err, res) => {
+//   if (err) {
+//     console.error("Error executing query", err.stack);
+//   } else {
+//     countries = res.rows;
+//     countryCodes = countries.map(obj => obj.country_code);
+//     console.log("rows are: ", countryCodes);
+//     // quiz = res.rows;
+//   }
+//   db.end();
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
-  res.render("index.ejs", { countries: countryCodes, total: 3 });
+  const result = await db.query("select country_code from visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(countries);
+  res.render("index.ejs", { countries : countries, total : countries.length});
+  // res.render("index.ejs", { countries: countryCodes, total: 3 });
+  db.end();
 });
 
 app.listen(port, () => {
